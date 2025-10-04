6201348efa8f145fb28753ac5715d0ae6cb79a6d
ibrahimwael951 <ibrahimwael809@gmail.com>
2025-10-04 10:20:53 +0300

    added nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   const menuItems = [
     { label: t("Home"), ariaLabel: t("Go to home page"), link: "/" },
     { label: t("About"), ariaLabel: t("Learn about us"), link: "/about" },
-    { label: t("Exams"), ariaLabel: t("View our services"), link: "/exams" },
+    { label: t("Level"), ariaLabel: t("View our services"), link: "/levels" },
     { label: t("Contact"), ariaLabel: t("Get in touch"), link: "/contact" },
     { label: t("Chat Bot"), ariaLabel: t("Get in touch"), link: "/Chat" },
   ];
